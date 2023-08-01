3818d7f2317cd111ef454f9ca89b7d7b75f8f09f
Joey Santana <joeysantana3@gmail.com>
2023-07-31 22:37:49 -0700

    troubleshooting whitenoise, possible typo

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+pip freeze
--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ if not DEBUG:
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whiteNoise.storage.CompressedManifestStaticFilesStorage'
     
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
